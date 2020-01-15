2bb1fc72666608a0a26403ff287967099f895160
jxlstudio <jeremy@jxl.io>
2020-01-14 19:17:42 -0500

    first commit

---
+++ src/components/LevelSelect.vue
@@ -0,0 +1,16 @@
+<template>
+    <select>
+        <option v-for="(level, index) in levels" :key="level" :value="getBaseDamage(index)" v-on:input="$emit('input', $event.target.value)">Level {{level}} | Base Dmg: {{getBaseDamage(index)}}</option>
+    </select>
+</template>
+
+<script>
+export default {
+  props: ['levels', 'damage'],
+  methods: {
+    getBaseDamage: function (id) {
+      return this.damage[id]
+    }
+  }
+}
+</script>
---
+++ src/views/WeaponList.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <table>
+      <tr>
+        <th>Name</th>
+        <th>Base Damage</th>
+        <th>Bloodied</th>
+      </tr>
+      <tr v-for="weapon in ranged" :key="weapon.name">
+        <td>{{ weapon.name }}</td>
+        <!-- <td><LevelSelect :levels="weapon.level" :damage="weapon.damage" v-model="baseDmg"/></td> -->
+        <td>
+          <ul>
+              <li v-for="(lvl, index) in weapon.level" :key="lvl">Level {{lvl}} - Dmg: {{weapon.damage[index]}}</li>
+          </ul>
+        </td>
+        <td>{{ baseDmg }}</td>
+      </tr>
+      <tr v-for="weapon in melee" :key="weapon.name">
+        <td>{{ weapon.name }}</td>
+        <td>{{ weapon.level }}<br>{{ weapon.damage }}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+import ranged from '@/assets/ranged.json'
+import rangedEffects from '@/assets/ranged-effects.json'
+import melee from '@/assets/melee.json'
+import meleeEffects from '@/assets/melee-effects.json'
+// import LevelSelect from '@/components/LevelSelect.vue'
+
+export default {
+  name: 'WeaponList',
+  components: {
+    // LevelSelect
+  },
+  data () {
+    return {
+      ranged: ranged.data,
+      rangedEffects: rangedEffects.data,
+      melee: melee.data,
+      meleeEffects: meleeEffects.data,
+      baseDmg: null
+    }
+  },
+  methods: {
+    getBaseDamage: function (id) {
+      return this.damage[id]
+    }
+  }
+}
+</script>
